26f707c8786da6fc1251926c829b29e0974249d1
Prakashchanchal <68770660+Prakashchanchal@users.noreply.github.com>
2020-08-24 03:09:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { createAppContainer, NavigationActions } from 'react-navigation';
+import { createStackNavigator,NavigationStackOptions } from 'react-navigation-stack';
+import AppRouter from './src/Navigation/navigation'
+import {Provider} from 'mobx-react'
+import store from './src/stores/Store'
+const App=()=>{
+    return(
+        <Provider store={store}>
+    <AppRouter/>
+    </Provider>
+    )};
+export default App;
+
